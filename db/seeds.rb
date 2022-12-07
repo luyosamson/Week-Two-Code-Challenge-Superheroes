c77825983a32d3c82e49efa1d3bc371b54879b08
luyosamson <luyosamson@gmail.com>
2022-12-07 10:48:57 +0300

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "seeding data ..."
+heroes = Hero.create([
+    {name: "Samson", super_name: "Sam"},
+    {name: "David", super_name: "Dev"},
+    {name: "Mary", super_name: "Maria"},
+    {name: "Emmanuel", super_name: "Ema"}
+    ])
+
+powers = Power.create([
+    { name: "Nguvu", description: "Energetic"},
+    { name: "Supa Power", description: "Powerfull"},
+    { name: "Indomitable", description: "Powerfull"}
+    ])
+
+heropowers = HeroPower.create([
+    {strength: "Strong", hero_id: 1,power_id: 1},
+    {strength: "Strong", hero_id: 2,power_id: 1},
+    {strength: "Average", hero_id: 3,power_id: 2},
+    {strength: "Weak", hero_id: 4,power_id: 3}
+    ])
+puts "done seeding"
