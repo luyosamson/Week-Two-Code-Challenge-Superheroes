afca6b5dc8f836c0730b1eb68aaeae6ef5addafe
luyosamson <luyosamson@gmail.com>
2022-12-07 10:03:02 +0300

    Modify migration

--- db/migrate/20221207033744_create_hero_powers.rb
+++ db/migrate/20221207033744_create_hero_powers.rb
@@ -2,6 +2,8 @@ class CreateHeroPowers < ActiveRecord::Migration[7.0]
   def change
     create_table :hero_powers do |t|
       t.string :strength
+      t.integer :hero_id
+      t.integer :power_id
 
       t.timestamps
     end
